e659df0c339762cb370f99c57176ef24ea957c5e
iamdanielyun <dsy3712@gmail.com>
2025-02-04 16:13:51 -0500

    added more forms

--- src/web/src/pages/CourseReviewsPage.vue
+++ src/web/src/pages/CourseReviewsPage.vue
@@ -23,6 +23,23 @@
             >
             </textarea>
         </div>
+        <div class="form-group">
+            <label for="rating">Rating:</label>
+            <select v-model="rating" class="form-control" id="rating">
+            <option value="5">5 - Excellent</option>
+            <option value="4">4 - Very Good</option>
+            <option value="3">3 - Good</option>
+            <option value="2">2 - Fair</option>
+            <option value="1">1 - Poor</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="result">Result:</label>
+            <p id="result">{{ result }}</p>
+        </div>
+        <button @click="fetchReviews()" class="btn btn-primary">Fetch Reviews</button>
+        <button @click="submitReview()" class="btn btn-success">Submit Review</button>
+        <button @click="clearForm()" class="btn btn-danger">Clear Form</button>
     </section>
   </b-container>
 </template>
