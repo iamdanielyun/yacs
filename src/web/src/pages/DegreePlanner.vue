f1297289883f26ea94846a8b4eddccd8d5671aa0
iamdanielyun <dsy3712@gmail.com>
2025-03-25 16:11:39 -0400

    started degree planner feature

---
+++ src/web/src/pages/DegreePlanner.vue
@@ -0,0 +1,20 @@
+<template>
+    <b-container class="mt-3">
+        <h2>Degree Progress Tracker</h2>
+    
+        <!-- Degree Selection -->
+        <div class="form-group">
+            <label for="degree">Select Degree:</label>
+            <select v-model="selectedDegree" class="form-control" id="degree">
+                <option v-for="degree in degrees" :value="degree.id" :key="degree.id">
+                    {{ degree.name }}
+                </option>
+            </select>
+        </div>
+
+        <!-- Progress Visualization -->
+        <div v-if="selectedDegree">
+            <!-- complete laterr -->
+        </div>
+    </b-container>
+</template>
