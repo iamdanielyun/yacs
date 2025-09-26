e641bf4aa5aedcb45e82282aec5c29694c9eb0b0
iamdanielyun <dsy3712@gmail.com>
2025-09-26 16:14:07 -0400

    started the frontend vue page

---
+++ src/web/src/pages/ProfessorReviews.vue
@@ -0,0 +1,20 @@
+<template>
+  <b-container class="mt-3">
+    <section id="professor-reviews">
+      <h2>Professor Reviews & Ratings</h2>
+      
+      <!-- Professor Search -->
+      <div class="form-group">
+        <label for="professorSearch">Search Professor:</label>
+        <input 
+          v-model="searchQuery" 
+          @input="searchProfessors" 
+          type="text" 
+          class="form-control" 
+          id="professorSearch" 
+          placeholder="Enter professor name or department"
+        />
+      </div>
+    </section>
+  </b-container>
+</template>
